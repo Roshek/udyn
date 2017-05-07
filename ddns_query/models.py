328bcc4e747eef6168ab41a72b18cf40fdba7ad8
Szabados Ákos <szabados.akos@gmail.com>
2017-05-07 14:28:24 +0200

    Alter models
    Use char field insted of url, because it atomatically adds http:// prefix to url.

--- ddns_query/models.py
+++ ddns_query/models.py
@@ -9,16 +9,21 @@ class User(AbstractUser):
 
 class Dyname(models.Model):
     prefix = models.CharField('domain prefix', max_length=15, unique=True)
-    zone = models.URLField("DNS zone", default='ddns.aszabados.eu')
-    primary_dns_host = models.URLField(
+    zone = models.CharField(
+        "DNS zone",
+        max_length=200,
+        default='ddns.aszabados.eu'
+    )
+    primary_dns_host = models.CharField(
         "Primary DNS server hostname",
+        max_length=200,
         default='ns1.aszabados.eu'
     )
     primary_dns_ip = models.GenericIPAddressField(
         'Primary DNS server IP',
         default='188.213.166.188'
     )
-    ip = models.GenericIPAddressField('current ip')
+    ip = models.GenericIPAddressField('current ip', default='0.0.0.0')
     mod = models.DateTimeField('last modified', auto_now=True)
     token = models.CharField('renewal token', max_length=50, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
